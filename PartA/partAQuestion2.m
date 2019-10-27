e9bfc15479a283c5bf24455e8c9ffdb772a28512
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-10-26 23:11:30 -0700

    fixed partAQuestion2 by making use of the other file functions.

--- PartA/partAQuestion2.m
+++ PartA/partAQuestion2.m
@@ -1,40 +1,18 @@
 function [D0y,w] = partAQuestion2()
+%calculates the derivative of f(x)
+%by using a D0 matrix.
 f = @(x) exp(sin(x));
 fp = 4*pi;
+n = 100;
 
-x = linspace(0,fp,100);
-%get w from last question
-w = partAQuestion1();
-y = zeros(length(x)-1,1);
-nmax = length(x)-1;
+%get w from centerDiff.m
+w = centerDiff(f,n,fp);
 
-%calculate the values of y
-for n = 1:nmax
-    y(n) = f(x(n));
-end
+%get y from calculateY.m
+y = calculateY(f,n,fp);
 
-%make vectors that are full of
-%nmax-1 1s and -1s
-vP = [];
-vN = [];
-for n = 1:nmax-1
-    vP(n) = 1;
-    vN(n) = -1;
-end
+%get D0 from calculateD0.m
+D0 = calculateD0(n-1);
 
-%make matrices with the vectors
-dP = diag(vP,1);
-dN = diag(vN,-1);
-%combine the matrices to make D0
-D0 = dP + dN;
-%add a 1 and -1 to the correct places in the matrix
-%to complete the D0 matrix.
-D0(nmax,1) = 1;
-D0(1,nmax) = -1;
-
-%multiply the D0 matrix and y vector, then
-%multiply each element by 1/2h.
-D0y = (D0*y);
-for n = 1: nmax
-    D0y(n) = D0y(n)/ (2 * (fp/n));
-end
+%get D0y from calculateD0y.m
+D0y = calculateD0y(D0,y,n,fp);
